0a6a7720e61bb186a9726d00f0edf16d41fe8ec9
chfpsheng <1018559223@qq.com>
2026-09-17 17:24:21 +0800

    少儿中医详情页展示参数
    showshaoer.php 调用 parameter_label 读取 module=16 的参数值，在详情页信息栏展示活动类型、主办单位、主办单位名称、是否亲子、是否免费、价格、人数。

--- templates/m1156ui010/showshaoer.php
+++ templates/m1156ui010/showshaoer.php
@@ -1,4 +1,11 @@
 <?php defined('IN_MET') or exit('No permission'); ?>
+<?php
+// 读取少儿中医（module=16）的参数值，用于详情页展示
+$_shaoer_para = array();
+if (!empty($data['id'])) {
+    $_shaoer_para = load::mod_class('parameter/parameter_label', 'new')->get_parameter_contents(16, $data['id'], $data['class1'], $data['class2'], $data['class3']);
+}
+?>
 <include file="head.php" page="showshaoer" />
 <section class="met-content animsition">
     <div class="container">
@@ -20,6 +27,13 @@
                         </if>
                         <span><i class="fa fa-ticket"></i>是否免费：{$data.is_free_text}</span>
                     </div>
+                    <if value="$_shaoer_para">
+                    <div class="info" style="margin-top:8px;">
+                        <list data="$_shaoer_para" name="$p">
+                        <span><i class="fa fa-tag"></i>{$p.name}：{$p.value}</span>
+                        </list>
+                    </div>
+                    </if>
                 </div>
                 <div class="met-editor lazyload clearfix">
                     <div class="editorlightgallery">{$data.content}</div>
